ecd514cf719e55e119b28ad78e003cd1f3ebc6eb
sarath <osarath265@gmail.com>
2020-11-15 15:50:39 +0530

    fulfillment rel code is added

--- index.js
+++ index.js
@@ -17,8 +17,40 @@ app.get('/favicon.ico',(req,res)=>{
     res.end();
 })
 
+
+// LEDs APP
+
+var leds = {red:false, blue:false, green:false, yellow:false};
+
 app.post('/leds',(req,res)=>{
     console.log(req.body);
+    let color =  req.body.queryResult.parameters.color;
+
+    if(leds[color] == undefined){
+        res.json({fulfillmentText:`${color} LED is not there`});
+    }
+    
+    // TURN ON INTENT
+    if(req.body.queryResult.intent.name == "projects/nodemcu-colour-led/agent/intents/0bf9c09d-4e30-4646-b500-ff73aa0f0ef1"){
+        if(leds[color]){
+            res.json({fulfillmentText:`${color} LED is already switched on`});
+        }
+        else{
+            res.json({fulfillmentText:`switching on ${color} LED`});
+            leds[color] = true;
+        }
+    }
+    else if(req.body.queryResult.intent.name == 'projects/nodemcu-colour-led/agent/intents/b830d60b-3abe-4cd7-907a-448d5ff40ead'){
+        if(!leds[color]){
+            res.json({fulfillmentText:`${color} LED is already switched off`});
+        }
+        else{
+            res.json({fulfillmentText:`switching off ${color} LED`});
+            leds[color] = false;
+        }
+    }
+    
+    res.json({fulfillmentText:"${}"})
     res.end();
 });
 
