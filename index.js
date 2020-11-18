8d0b5bed069835bea5ff6d26654f9a991c18ce07
sarath <osarath265@gmail.com>
2020-11-18 08:38:09 +0530

    socket remote is added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { actionsSDK } = require('actions-on-google');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(cors());
 // parse application/x-www-form-urlencoded
@@ -10,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
 
 
 app.get('/favicon.ico', (req, res) => {
@@ -87,10 +90,64 @@ app.get('/leds', (req, res) => {
 
 })
 
+app.get('/leds/reset',(req,res)=>{
+    leds = { red: false, blue: false, green: false, orange: false };
+    res.end("reset complete");
+});
+
 app.get('/', (req, res) => {
     console.log("got a get request");
     res.end();
 })
 
 
-app.listen(process.env.PORT || 3300, () => { console.log("server is up!") });
+
+
+
+//RC CAR
+
+io.on('connection',(socket)=>{
+    console.log('client is connected');
+
+    socket.on('move-front',()=>{
+        console.log('front');
+        socket.broadcast.emit('move-front');
+    });
+
+    socket.on('move-back',()=>{
+        console.log('back');
+        socket.broadcast.emit('move-back');
+    });
+
+    socket.on('turn-left',()=>{
+        console.log('left');
+        socket.broadcast.emit('turn-left');
+    });
+
+    socket.on('turn-right',()=>{
+        console.log('right');
+        socket.broadcast.emit('turn-right');
+    });
+
+    socket.on('rotate-left',()=>{
+        console.log('rotate-left');
+        socket.broadcast.emit('rotate-left');
+    });
+
+    socket.on('rotate-right',()=>{
+        console.log('rotate-right');
+        socket.broadcast.emit('rotate-right');
+    });
+
+    socket.on('stop',()=>{
+        console.log("STOP");
+        socket.broadcast.emit('stop');
+    });
+
+    socket.on('disconnect',()=>{
+        console.log("client is disconnected");
+    });
+
+});
+
+http.listen(process.env.PORT || 3300, () => { console.log("server is up!") });
